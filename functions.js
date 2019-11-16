d6ef8fbb899b445929ae948e695bd138d7cba830
pacsal <pm93web@gmail.com>
2019-11-17 01:54:19 +0300

    Level 2
    Customer/ShopKeeper can select items shoped, and amount billed will be filled automaticall.
    
    ShopeKeeper will put amount given by customer and press calculate.
    
    Shopkeeper will get exactly change to be issued and how many coins or note should be issued.
    
    //this is done by javascript and html5

--- functions.js
+++ functions.js
@@ -1,25 +1,26 @@
 //function for TheShopkeeperChangeChallenge
 function calculate(){
+	//capture the iput values and find the difference
 	var getAmountBilled = document.getElementById("bill").value;
 	var getAmountPaid = document.getElementById("pay").value;
 	var getChange = getAmountPaid - getAmountBilled;
 
-//echo the values of amount billed and paid to specified output ids
+	//echo the values of amount billed and paid to specified output ids
 	document.getElementById("billed").innerHTML = "<h3>Amount Billed</h3>"+ getAmountBilled;
 	document.getElementById("paid").innerHTML = "<h3>Amount Paid</h3>"+getAmountPaid;
 
 	//calculate the change afte cutomer paid
 	document.getElementById("actualChange").innerHTML = "<h3>Change</h3>"+getChange;
 
+	//coins and notes function
 	function coinsNote(){
+		//coins and notes value arrays
 		var noteAndCoins = [1000, 500, 100, 50, 20, 10, 5, 1];
-		//var coins = [20, 10, 5, 1];
-		//document.getElementById("note").innerHTML = getChange%500;
 
-		var NumNote = 0;
+		var NumNote = 0;            //init default ans for result before devision calculation
 		var Reminder = getChange;
-		var howMany = [];
-		var coinNote = [];
+		var howMany = [];           //init default array for exactly number of note/coin
+		var coinNote = [];          //init default array for indicating a note/coin
 		//lets find how many coins and notes
 		for(i=0; i<noteAndCoins.length; i++){
 			NumNote = Reminder / noteAndCoins[i];
@@ -28,7 +29,8 @@ function calculate(){
 			coinNote[i] = ((i < 4) ? 'Note[s]' : 'Coin[s]');
 			howMany[i] = ~~NumNote;
 		}
-
+		//display only coins and notes which are divisible and will sum the change
+		document.getElementById("desc").innerHTML = "";
 		for(i=0; i<noteAndCoins.length; i++){
 			if(howMany[i]>0){
 				document.getElementById("desc").innerHTML += noteAndCoins[i]+"=>"+howMany[i]+" "+coinNote[i]+"<br>";
@@ -41,10 +43,12 @@ function calculate(){
 }
 
 var totalAmount = 0;
-
-function checkedItem(itemAmount){
+//incement the selected items
+function checkedItem(checkbox,itemAmount){
 	var amount = itemAmount;
-	totalAmount  += amount;
+		//check if item is selected or unselected and encrement or decrement respectively
+		if(checkbox.checked == true){totalAmount += amount;}else{totalAmount -= amount;}
+	//echo bill amount on bill input form
 	document.getElementById("bill").value = totalAmount;
 }
   			
