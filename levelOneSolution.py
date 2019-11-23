048e5f4ff873995f55f3c767a78da765840cf6b7
Bakley <barclaykoin@gmail.com>
2019-11-23 15:36:27 +0300

    User thinking chnages

--- levelOneSolution.py
+++ levelOneSolution.py
@@ -1,39 +1,37 @@
 import math
 
-inputValue = int(input('Enter a value: '))
+inputValue = int(input('Enter change due: '))
 
 outputString = ''
 
 
-def evaluate(amountToEvaluate, valueToEvaluate, singleNoteString, pluralNoteString):
+def evaluate(amountToEvaluate, valueToEvaluate, changeText):
     'Function to evaluate the change'
     inInput = math.floor(amountToEvaluate / valueToEvaluate)
-    outputString = ""
+    outputString = str(inInput) + " " + changeText
     if inInput > 1:
-        outputString = str(inInput) + " " + pluralNoteString
-    else:
-        outputString = str(inInput) + " " + singleNoteString
-
+        outputString += "s"
     return outputString
 
+
 if inputValue >= 100000:
-    outputString += evaluate(inputValue, 100000, "hundred thousand note", "hundred thousands notes")
+    outputString += evaluate(inputValue, 100000, "hundred thousand note")
     inputValue = inputValue % 100000
 if inputValue >= 10000:
-    outputString += evaluate(inputValue, 10000, "ten thousand note", "ten thousands notes")
+    outputString += evaluate(inputValue, 10000, "ten thousand note")
     inputValue = inputValue % 10000
 if inputValue >= 1000:
-    outputString += evaluate(inputValue, 1000, "thousand note", "thousands notes")
+    outputString += evaluate(inputValue, 1000, "thousand note")
     inputValue = inputValue % 1000
 if inputValue >= 100:
-    outputString += " " + evaluate(inputValue, 100, "hundred note", "hundreds notes")
+    outputString += " " + evaluate(inputValue, 100, "hundred note")
     inputValue = inputValue % 100
 if inputValue >= 50:
-    outputString += " " + evaluate(inputValue, 50, "fifty note", "fifties notes")
+    outputString += " " + evaluate(inputValue, 50, "fifty note")
     inputValue = inputValue % 50
 if inputValue >= 10:
-    outputString += " " + evaluate(inputValue, 10, "ten coin", "ten coins")
+    outputString += " " + evaluate(inputValue, 10, "ten coin")
     inputValue = inputValue % 10
 if inputValue > 0:
-    outputString += " " + evaluate(inputValue, 1, "coin", "coins")
+    outputString += " " + evaluate(inputValue, 1, "coin")
 print(outputString)
