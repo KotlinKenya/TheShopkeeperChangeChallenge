073860d67798de0c9f68d1e52d3cdd876468fb9e
jovyinny <jovinemutelani@gmail.com>
2023-02-06 13:18:21 +0300

    added solution 2

--- shopkeeper challenge.py
+++ shopkeeper challenge.py
@@ -23,7 +23,39 @@ class Shopkeeper():
         except Exception as error:
                 logging.error(f"{error}")
     
+    def level_2(self,shopping_cart:dict):
+        """
+        >>> Shopkeeper().level_2({"Milk":2,"Honey":1})
+        Your Total bill is 250
+        Enter amount to pay: 255
+        The Change is 5
+        DENOMINATION
 
+        1 5 coin
+
+        """
+        # A dictionary of product(key) and price (value) found in the shop
+        product_and_price:dict = {"Milk":44,"Honey":162,"Eggs":357,"Sread":41, "Spinach": 42,"Towel":236,"Soda":65}
+        total_cost:int = 0 #total cost of what the customer shops
+        """
+        Moving through customer cart to get items and quantity. 
+        Then getting total cost per each item. adding up all costs to obtain accumulated cost stored as total_cost
+        """
+        try:
+            for item in shopping_cart.keys():
+                total_cost+=product_and_price[item]*shopping_cart[item]
+            
+            print(f"Your Total bill is {total_cost}")
+            customer_cash = int(input("Enter amount to pay: "))
+            change=customer_cash-total_cost
+            if change>0:
+                print(f"The Change is {change}\nDENOMINATION")
+                self.level_1(change)
+                return
+            print(f"No change\nAdd more cash worth {abs(change)}")
+
+        except Exception as error:
+            logging.error(f"{error}")
         
 
 # shop=Shopkeeper()
